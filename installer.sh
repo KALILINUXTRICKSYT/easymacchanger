8b635f9a605f85e282728d98b4db23ec767024e2
KALI LINUX TRICKS <42170844+KALILINUXTRICKSYT@users.noreply.github.com>
2018-12-31 10:55:57 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,53 @@
+chmod +x easymacchanger
+cp -r easymacchanger /usr/local/sbin
+echo "rm -rf /usr/local/sbin/easymacchanger && echo -e '\e[1;31m    
+
+                       (UNINSTALLATION COMPLETED)
+
+\e[0m' && rm uninstaller.sh " > uninstaller.sh 
+
+
+resize -s 24 95
+
+clear
+
+echo -e '\e[1;32m
+     ______                    __  ___                       
+    / ____/___ ________  __   /  |/  /___ ______    \e[1;34mChange the network  \e[1;32m  
+   / __/ / __ `/ ___/ / / /  / /|_/ / __ `/ ___/    \e[1;34midentity of your device. \e[1;32m
+  / /___/ /_/ (__  ) /_/ /  / /  / / /_/ / /__               
+ /_____/\__,_/____/\__, /  /_/  /_/\__,_/\___/          
+                  /____/                            \e[1;31mEasy Mac Changer is intented\e[1;32m
+    ________                                        \e[1;31mONLY FOR EDUCATIONAL PURPOSES.\e[1;32m
+   / ____/ /_  ____ _____  ____ ____  _____
+  / /   / __ \/ __ `/ __ \/ __ `/ _ \/ ___/                          
+ / /___/ / / / /_/ / / / / /_/ /  __/ /             \e[1;37mCreated by "KALI LINUX TRICKS"      \e[1;32m   
+ \____/_/ /_/\__,_/_/ /_/\__, /\___/_/  v1.1        \e[1;37mhttps://www.youtube.com/c/KALILINUXTRICKS \e[1;32m
+                        /____/         (Linux)   
+\e[1;36m
+                      
+
+                              (INSTALLATION COMPLETED)
+
+
+                                                             \e[1;32m
+  To execute Easy Mac Changer type anywhere in your terminal "easymacchanger".  \e[0m 
+
+'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
